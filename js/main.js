f6cf43b185aaebf8797f4679583192405c6462f9
Matt McDonald <mcdonamj087@gmail.com>
2020-06-13 21:58:51 -0500

    Finished travelers and hospitality companies pages

--- js/main.js
+++ js/main.js
@@ -20,11 +20,13 @@ window.addEventListener('click', e => {
   const target = e.target;
   if(['mobile-nav-trigger', 'mobile-nav-close-trigger', 'mobile-nav--underlay'].includes(target.id)) {
 
+    // Lock the body from scrolling if mobile nav is open
     if(document.body.classList.contains('mobile-nav-is-open')) {
       bodyScrollLock.enableBodyScroll(mobileNav);
     } else {
       bodyScrollLock.disableBodyScroll(mobileNav);
     }
+
     toggleMobileNav();
   }
 })
@@ -33,20 +35,7 @@ function toggleMobileNav() {
   document.body.classList.toggle('mobile-nav-is-open');
 }
 
-
-
-// // 2. Get a target element that you want to persist scrolling for (such as a modal/lightbox/flyout/nav).
-// // Specifically, the target element is the one we would like to allow scroll on (NOT a parent of that element).
-// // This is also the element to apply the CSS '-webkit-overflow-scrolling: touch;' if desired.
-// const targetElement = document.querySelector('#someElementId');
-
-// // 3. ...in some event handler after showing the target element...disable body scroll
-// disableBodyScroll(targetElement);
-
-// // 4. ...in some event handler after hiding the target element...
-// enableBodyScroll(targetElement);
-
-
+// Headroom Initialization - Makes the top nav hide and show depending on scroll direction
 const header = document.getElementById("master-header");
 var headroom  = new Headroom(header, {
   offset: 80,
